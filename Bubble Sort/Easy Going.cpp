dbda09376be96cca6a340d5c54fffa4c92e1fdc2
Anustup Mukherjee <60361231+Anustup900@users.noreply.github.com>
2020-08-22 12:55:17 +0530

    Update Easy Going.cpp

--- Bubble Sort/Easy Going.cpp
+++ Bubble Sort/Easy Going.cpp
@@ -1,17 +1,40 @@
 #include <iostream>
 using namespace std ;
-int main(){
-    int T;
-    cin >>T;
-    int a1,a2,a3,a4,a5;
-    cin>>a1>>a5;
-    cin>>a1>>a2>>a3>>a4>>a5;
-    int d= (a5-a1);
-    int e=(a1+a2+a3+a4);
-    int f=(a2+a3+a4+a5);
-    int g=(f-e);
-    if(g==d){
-        cout<<d<<"";
-        
-    }
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n,m,temp,max_sum=0,min_sum=0;
+		cin>>n>>m;
+		int arr[n];
+		for(int i=0;i<n;i++)
+		{
+			cin>>arr[i];
+		}
+		for(int i=0;i<n;i++)
+		{
+			for(int k=0;k<n-i-1;k++)
+			{
+				if(arr[k]>arr[k+1])
+				{
+					temp=arr[k];
+					arr[k]=arr[k+1];
+					arr[k+1]=temp;
+				}
+			}
+		}
+		for(int i=m;i<n;i++)
+		{
+			max_sum+=arr[i];
+		}
+		for(int i=0;i<n-m;i++)
+		{
+			min_sum+=arr[i];
+		}
+		int result=abs(max_sum-min_sum);
+		cout<<result<<endl;
+	}
+return 0;
 }
